52a98e51d58708ccc0d626dd5b4f8f5cb6b3e2f3
rbhuma2 <rbhuma2@IN-PF2HX3LV>
2021-04-10 21:14:14 +0530

    updated latest code

--- src/app/pages/create-token/create-token.component.ts
+++ src/app/pages/create-token/create-token.component.ts
@@ -4,6 +4,9 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { StripeService, StripeCardComponent } from 'ngx-stripe';
 import { StripeCardElementOptions, StripeElementsOptions } from '@stripe/stripe-js';
 import { CreateTokenService } from './create-token.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from 'src/app/api.service';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-create-token',
@@ -11,8 +14,13 @@ import { CreateTokenService } from './create-token.service';
   styleUrls: ['./create-token.component.less']
 })
 export class CreateTokenComponent implements OnInit {
+  amount : number;
+  identifier : any;
+  isProcessed : boolean;
+  transaction_id : any;
+  totalAmount : any;
   @ViewChild(StripeCardComponent) card: StripeCardComponent;
-
+  
   cardOptions: StripeCardElementOptions = {
     style: {
       base: {
@@ -35,15 +43,29 @@ export class CreateTokenComponent implements OnInit {
   stripeTest: FormGroup;
 
   constructor(
+    private http: HttpClient,
     private fb: FormBuilder, 
     private stripeService: StripeService,
-    private createTokenService: CreateTokenService
+    private createTokenService: CreateTokenService,
+    private _ApiService: ApiService,
+    private _Router: Router
   ) {}
 
   ngOnInit(): void {
+    
+    this.setAmount();
     this.stripeTest = this.fb.group({
       name: ['', [Validators.required]]
     });
+  }
+
+  private setAmount (){
+    this._ApiService.cart('cart').subscribe(res=>{
+      this.amount = res.totalAmount;
+      this.identifier = res.identifier;
+     //console.log(res);
+    })    
+    
   }
 
   createToken(): void {
@@ -54,22 +76,35 @@ export class CreateTokenComponent implements OnInit {
         if (result.token) {
           /** Stripe generates a token id from the checkout */
           let requestObject = {
-            "transactionId"   : "ch_1IdH0dHjDycGB3bIQDuXT6bY",
-            "amount"          : 1000,
-            "links"           : [],
-            "token"           : result.token,
-            "tokenId"         : result.token.id
+            "amount"          : this.amount,
+            "stripeToken"     : result.token.id,
+            "id"              : this.identifier
           }
 
           /** Sending the generated token and price to the backend charges API to complete the transaction */
           this.createTokenService.sendPaymentInfoToBackend(requestObject).subscribe(response => {
-            console.log("Successfully sent to server");
-            console.log(response);
-          });
-        } else if (result.error) {
+            /*if(response) {
+              this.transaction_id = response.transaction_id;
+              this.isProcessed = true;
+            }
+
+            let requestObjectpayment = {
+              "transaction_id" :  this.transaction_id,
+              "isProcessed"    : this.isProcessed,
+              "id"             : this.identifier
+              }
+         
+          this.createTokenService.sendPaymentConfirmation(requestObjectpayment).subscribe(response => {
+              
+          });*/
+          console.log(response);
+          this._Router.navigate(['/menu']);
+
+        });
+       } else if (result.error) {
           // Error creating the token
           console.log(result.error.message);
         }
       });
-  }
+    }
 }
--- src/app/pages/create-token/create-token.service.ts
+++ src/app/pages/create-token/create-token.service.ts
@@ -13,4 +13,9 @@ export class CreateTokenService {
   public sendPaymentInfoToBackend(payload) {
     return this.http.post<any>("http://localhost:9090/v1/charge", payload);
   }
+
+  /*public sendPaymentConfirmation(payload) {
+    // url : this.sendPaymentConfirmation.requestObjectpayment
+  return this.http.patch<any>("http://localhost:9090/v1/cart/"+CreateTokenService.arguments(this.sendPaymentConfirmation[2].toString), payload);
+}*/
 }
